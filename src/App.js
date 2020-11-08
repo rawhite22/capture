5695d90bdf36c118039c0e6419b83cf3bf514362
rawhite22 <robert.a.white625@gmail.com>
2020-11-08 16:49:33 -0500

    9. React Router Addition

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React from 'react';
 // global style
 import GlobalStyle from './components/GlobalStyle';
 // pages
-import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+// routing
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path='/'>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
