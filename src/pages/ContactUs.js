13f6b6a40db3b066607d9d44bf7de977c7ca13c4
rawhite22 <robert.a.white625@gmail.com>
2020-11-11 13:00:31 -0500

    21. Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,17 +1,68 @@
-import React from 'react'
+import React from 'react';
 // animation
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnimation } from '../animation';
+import styled from 'styled-components';
 
 const ContactUs = () => {
   return (
-    <motion.div  variants={pageAnimation}
+    <ContactStyle
+      style={{ background: '#fff' }}
+      variants={pageAnimation}
       initial='hidden'
       animate='show'
       exit='exit'>
-      <h1>Contact</h1>
-    </motion.div>
-  )
-}
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send us a message</h2>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send us a message</h2>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send us a message</h2>
+        </Social>
+      </Hide>
+    </ContactStyle>
+  );
+};
 
-export default ContactUs
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  height: 3rem;
+  width: 3rem;
+  background-color: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
+
+export default ContactUs;
